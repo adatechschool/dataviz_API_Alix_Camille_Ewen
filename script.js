46b6f49a2d16da37b268c02a25a4f180d4f44656
Pressense Fasani <camillefasani@gmail.com>
2025-03-18 16:37:23 +0100

    Fonctionnalité Message accueil + Horloge + début style

--- script.js
+++ script.js
@@ -17,4 +17,52 @@ const indexMois = nouvelleDate.getMonth();
 const moisDelAnnee = listeMois[indexMois];
 
 //On affiche la date du jour dans le header
-dateDuJour.innerHTML = `${jourDeLaSemaine} ${jourDuMois} ${moisDelAnnee} chez Ada !`
+dateDuJour.innerHTML = `${jourDeLaSemaine} ${jourDuMois} ${moisDelAnnee} chez`
+
+// On récupère la class container dans le HTML
+let messageJournee = document.querySelector("#message")
+
+//  On recupère les heures de la date du jour
+let heureActuelle = nouvelleDate.getHours()
+
+function messageAccueil() {
+    // On déclare la variable dans la fonction
+    let greeting ;
+    if (heureActuelle >= 8 && heureActuelle <12) {
+        greeting = "Bon matin !"
+    } else if (heureActuelle >=12 && heureActuelle <14) {
+        greeting = "Bon appétit !"
+    } else if (heureActuelle >=14 && heureActuelle <18){
+        greeting = "Bon après-midi !"
+    } else if (heureActuelle >=18 && heureActuelle <19){
+        greeting = "Bonne soirée !"
+    } else {
+        console.log("c'est la nuit")
+        greeting = ""
+    }
+    // On ajoute le txt au HTML
+    messageJournee.innerHTML = `<h1>${greeting}</h1>`
+}
+messageAccueil()
+//setInterval(messageAccueil, 60000);
+
+// Horloge digitale
+function afficherHeure() {
+
+    let minutes = nouvelleDate.getMinutes();
+    let secondes = nouvelleDate.getSeconds();
+    
+    if (heureActuelle < 10) {
+      heureActuelle = "0" + heureActuelle;
+    } else if (minutes < 10) {
+      minutes = "0" + minutes;
+    } else if (secondes < 10) {
+      secondes = "0" + secondes;
+    }
+    
+    document.getElementById("hour").innerHTML = `${heureActuelle}:`;
+    document.getElementById("minute").innerHTML = `${minutes}:`;
+    document.getElementById("second").innerHTML = `${secondes}`;
+  }
+  
+  setInterval(afficherHeure, 500);
