b19776e42c7d7964f61ba507d31321188e71e359
Pressense Fasani <camillefasani@gmail.com>
2025-03-20 10:14:10 +0100

    Appel API fonctionnel + affichage données

--- script.js
+++ script.js
@@ -24,99 +24,87 @@ let messageJournee = document.querySelector("#message")
 function messageAccueil() {
   const aujourdhui = new Date()
   let heureActuelle = aujourdhui.getHours()
-    // On déclare la variable dans la fonction
-    let message ;
-    if (heureActuelle >= 8 && heureActuelle <12) {
-        message = "Bon matin !"
-    } else if (heureActuelle >=12 && heureActuelle <14) {
-        message = "Bon appétit !"
-    } else if (heureActuelle >=14 && heureActuelle <18){
-        message = "Bon après-midi !"
-    } else if (heureActuelle >=18 && heureActuelle <19){
-        message = "Bonne soirée !"
-    } else {
-        console.log("c'est la nuit")
-        message = ""
-    }
-    // On ajoute le txt au HTML
-    messageJournee.innerHTML = `<h1>${message}</h1>`
+  // On déclare la variable dans la fonction
+  let message ;
+  if (heureActuelle >= 8 && heureActuelle <12) {
+    message = "Bon matin !"
+  } else if (heureActuelle >=12 && heureActuelle <14) {
+    message = "Bon appétit !"
+  } else if (heureActuelle >=14 && heureActuelle <18){
+    message = "Bon après-midi !"
+  } else if (heureActuelle >=18 && heureActuelle <19){
+    message = "Bonne soirée !"
+  } else {
+    console.log("c'est la nuit")
+    message = ""
+  }
+  // On ajoute le txt au HTML
+  messageJournee.innerHTML = `<h1>${message}</h1>`
 }
 messageAccueil()
 setInterval(messageAccueil, 60000);
 
 // Horloge digitale
 function afficherHeure() {
-    const aujourdhui = new Date()
-    let heures = aujourdhui.getHours()
-    let minutes = aujourdhui.getMinutes();
-    let secondes = aujourdhui.getSeconds();
-    
-    if (heures < 10) {
-      heures = "0" + heures;
-    } 
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    } 
-    if (secondes < 10) {
-      secondes = "0" + secondes;
-    }
-    
-    document.getElementById("hour").innerHTML = `${heures}:`;
-    document.getElementById("minute").innerHTML = `${minutes}:`;
-    document.getElementById("second").innerHTML = `${secondes}`;
+  const aujourdhui = new Date()
+  let heures = aujourdhui.getHours()
+  let minutes = aujourdhui.getMinutes();
+  let secondes = aujourdhui.getSeconds();
+  
+  if (heures < 10) {
+    heures = "0" + heures;
+  } 
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  } 
+  if (secondes < 10) {
+    secondes = "0" + secondes;
   }
   
+  document.getElementById("hour").innerHTML = `${heures}:`;
+  document.getElementById("minute").innerHTML = `${minutes}:`;
+  document.getElementById("second").innerHTML = `${secondes}`;
+}  
 setInterval(afficherHeure, 500);
 
-  // Barre de progression
-  function barreProgression(){
-    const aujourdhui = new Date()
-    const heureOuverture = new Date()
-    heureOuverture.setHours(8, 30)
-    let taux = aujourdhui-heureOuverture
-    let heures = aujourdhui.getHours()
-    let minutes = aujourdhui.getMinutes();
-    if((heures >=8 && minutes >=30) || (heures <=18 && minutes <=30)) {
-      let barreProgression = 0;
-      barreProgression = Math.round(100*taux/36000000)
-      document.getElementById("progressBar").style.width = barreProgression + "%";
-    } 
-  }
-  barreProgression()
-
-  // Requête API
-
-// On récupère le button / l'image ect..
-
-let bicloo = document.querySelector("#bicloo")
-
-
-// On créé la function changePokemon 
-    // la const est = à la fonction qui va faire la requête vers l'API
-
-const appelApi = async() => {
-   
-    //  Gerer de façon dynamique la requête vers l'URL en injectant le randomNumber
-    let requete = "https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_disponibilite-temps-reel-velos-libre-service-naolib-nantes-metropole/records?where=name%3D%22MOUTONNERIE%22&limit=1"; 
-
-    //  On stock la réponse de la requête dans data
-    let data = await fetch(requete)
-    //  On vérif avec console.log
-    console.log(data)
-
-    //   On transforme la réponse au format JSON
-    let response = await data.json()
-    //  On vérifie avec console.log
-    console.log(response)
-
-    const { name } = response.results[0];
-
-  bicloo.textContent = name
-
-
+// Barre de progression
+function barreProgression(){
+  const aujourdhui = new Date()
+  const heureOuverture = new Date()
+  heureOuverture.setHours(8, 30)
+  let taux = aujourdhui-heureOuverture
+  let heures = aujourdhui.getHours()
+  let minutes = aujourdhui.getMinutes();
+  
+  if((heures >=8 && minutes >=30) || (heures <=18 && minutes <=30)) {
+    let barreProgression = 0;
+    barreProgression = Math.round(100*taux/36000000)
+    document.getElementById("progressBar").style.width = barreProgression + "%";
+  } 
 }
-// Pour initialiser le jeu on appelle la fonction
+barreProgression()
+setInterval(barreProgression, 500000)
+
+// Requête API
+let station = document.querySelector("#station")
+let adresseStation = document.querySelector("#adresseStation")
+let velosDispo = document.querySelector("#velosDispo")
+let placesDispo = document.querySelector("#placesDispo")
+
+async function appelApi() {
+  let requete = "https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_disponibilite-temps-reel-velos-libre-service-naolib-nantes-metropole/records?where=name%3D%22MOUTONNERIE%22&limit=1"; 
+  let data = await fetch(requete)
+  let response = await data.json()
+
+  const { name, available_bike_stands, available_bikes, address } = response.results[0];
+
+  station.textContent = `Station : ${name}`
+  adresseStation.textContent = `Adresse : ${address}`
+  velosDispo.textContent = `En ce moment, il y a ${available_bikes} vélos disponibles !`
+  placesDispo.textContent = `En ce moment, il y a ${available_bike_stands} places disponibles !`
+}
 appelApi()
+
 
 
 
